d653cb638ee9abe8e853d1a56188d56df5345a17
Klamist <33776012+Klamist@users.noreply.github.com>
2026-05-25 00:44:17 +0800

    Add files via upload

--- QuieterFaceshieldBreathing/SelfBreathVolume.cs
+++ QuieterFaceshieldBreathing/SelfBreathVolume.cs
@@ -12,14 +12,21 @@ namespace SelfBreathVolume
     public class MainPlugin : BaseUnityPlugin
     {
         internal static ConfigEntry<int> Volume;
+        internal static ConfigEntry<int> V_hurt;
         internal static EPhraseTrigger LastTrigger;
 
         private void Awake()
         {
             Volume = Config.Bind(
                 "General",
-                "Volume Percent",
-                40,
+                "Breath Volume Percent",
+                30,
+                new ConfigDescription("Only affect yourself", new AcceptableValueRange<int>(0, 100))
+            );
+            V_hurt = Config.Bind(
+                "General",
+                "Hurt Volume Percent",
+                50,
                 new ConfigDescription("Only affect yourself", new AcceptableValueRange<int>(0, 100))
             );
 
@@ -62,27 +69,35 @@ namespace SelfBreathVolume
             if (clip == null)
                 return;
 
+            float factor = 1f;
+
             switch (MainPlugin.LastTrigger)
             {
+                // 呼吸类
                 case EPhraseTrigger.OnBreath:
-                case EPhraseTrigger.LegBroken:
-                case EPhraseTrigger.Bleeding:
                 case EPhraseTrigger.Dehydrated:
                 case EPhraseTrigger.Exhausted:
+                    factor = MainPlugin.Volume.Value * 0.01f;
+                    break;
+
+                // 受伤类
+                case EPhraseTrigger.Bleeding:
+                case EPhraseTrigger.Hit:
                 case EPhraseTrigger.HurtLight:
                 case EPhraseTrigger.HurtMedium:
                 case EPhraseTrigger.HurtHeavy:
                 case EPhraseTrigger.HurtNearDeath:
-                case EPhraseTrigger.Hit:
+                case EPhraseTrigger.LegBroken:
                 case EPhraseTrigger.OnAgony:
                 case EPhraseTrigger.OnBeingHurt:
                 case EPhraseTrigger.OnBeingHurtDissapoinment:
-                    break; // 允许继续执行
+                    factor = MainPlugin.V_hurt.Value * 0.01f;
+                    break;
+
                 default:
-                    return; // 其他情况直接退出
+                    return; // 不处理其他语音
             }
 
-            float factor = MainPlugin.Volume.Value * 0.01f;
             clip.Volume = factor;
         }
     }
